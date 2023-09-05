9f4903978a0f97c41406c51e9de887b2abff9679
Thalles <thalles.lossurdo@invenit.com.br>
2023-09-05 14:02:15 -0300

    chore: add login with client credentials

--- server/routing-api/src/infra/broker/platform-client.ts
+++ server/routing-api/src/infra/broker/platform-client.ts
@@ -1,26 +1,21 @@
 import platformClient, { ApiClientClass, RoutingApi } from 'purecloud-platform-client-v2'
 
 export class PlatformClient {
+  private readonly CLIENT_ID = ''
+  private readonly CLIENT_SECRET = ''
   private instance: ApiClientClass | undefined
   private routingApi: RoutingApi | undefined
 
-  setup(): void {
+  async setup(): Promise<void> {
     this.instance = platformClient.ApiClient.instance
     this.instance.setEnvironment('us-east-1')
     this.routingApi = new platformClient.RoutingApi()
-  }
-
-  getInstance(): ApiClientClass {
-    if (!this.instance) {
-      this.setup()
-    }
-
-    return this.instance as ApiClientClass
+    await this.instance.loginClientCredentialsGrant(this.CLIENT_ID, this.CLIENT_SECRET)
   }
 
   getRoutingApi(): RoutingApi {
     if (!this.routingApi) {
-      this.setup()
+      this.routingApi = new platformClient.RoutingApi()
     }
 
     return this.routingApi as RoutingApi
