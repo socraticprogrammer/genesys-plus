15b4f2325548280772660d52aac14db41c74a719
Thalles <thalles.lossurdo@invenit.com.br>
2023-09-11 10:14:34 -0300

    chore: add genesys regions

---
+++ server/routing-api/src/domain/models/genesys-region.ts
@@ -0,0 +1,4 @@
+export enum GenesysRegion {
+  'us-east-1' = 'mypurecloud.com',
+  'sa-east-1' = 'sae1.pure.cloud'
+}
--- server/routing-api/src/main/config/env.ts
+++ server/routing-api/src/main/config/env.ts
@@ -1,6 +1,10 @@
+import { GenesysRegion } from '@/domain/models'
+
 export const env = {
   GENESYS_CLIENT_ID: process.env.GENESYS_CLIENT_ID || '',
   GENESYS_CLIENT_SECRET: process.env.GENESYS_CLIENT_SECRET || '',
-  GENESYS_REGION: process.env.GENESYS_REGION || '',
+  GENESYS_REGION: process.env.GENESYS_REGION
+    ? GenesysRegion[process.env.GENESYS_REGION as 'us-east-1' | 'sa-east-1']
+    : GenesysRegion['us-east-1'],
   PORT: process.env.PORT || 3001
 }
