d81201b7bfd70fc85fc127a5b82d1c86916d8a67
Thalles <thalles.lossurdo@invenit.com.br>
2023-09-19 15:32:38 -0300

    fix: remove export users with filter by email

--- server/users-api/src/main/routes/user-routes.ts
+++ server/users-api/src/main/routes/user-routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import { makeListUsersRoute, makeListUsersWithFilterByEmailRoute } from '../factories/routes'
+
+const upload = multer()
 
 export default (router: Router): void => {
   router.get('/users', makeListUsersRoute())
   router.get('/users/export', makeListUsersRoute(true))
-  router.post('/users/filters/email', makeListUsersWithFilterByEmailRoute())
-  router.post('/users/filters/email/export', makeListUsersWithFilterByEmailRoute(true))
+  router.post('/users/filters/email', upload.single('file'), makeListUsersWithFilterByEmailRoute())
 }
