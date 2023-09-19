8de1641fe4fe219ff9631a291f81c0e3f6cc1d51
Thalles <thalles.lossurdo@invenit.com.br>
2023-09-19 17:58:41 -0300

    refactor: check name is different of undefined

--- server/routing-api/src/presentation/adapters/request/create-queues-request-adapter.ts
+++ server/routing-api/src/presentation/adapters/request/create-queues-request-adapter.ts
@@ -64,7 +64,7 @@ export class CreateQueuesRequestAdapter implements RequestAdapter<CreateQueues.P
         sourceQueueId
       }) => {
         return {
-          name: name.trim(),
+          name: name?.trim(),
           ...(divisionId
             ? {
                 division: {
